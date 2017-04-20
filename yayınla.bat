6ab649568be9fc057ac515dd133558f448478891
Hakan Çelik <hakancelik96@outlook.com>
2017-04-21 01:22:41 +0300

    Delete yayınla.bat

--- yayınla.bat
+++
@@ -1,5 +0,0 @@
-git init
-git add .
-git commit -m "This is a file or folder search engine made by python"
-git remote add origin https://github.com/hakancelik96/pfsearch.git
-git push -u origin master
